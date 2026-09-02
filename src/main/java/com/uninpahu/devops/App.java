3311cd7688675c1496875935aca07b6b5b6c0449
andresbry2020 <brayangarnica51@gmail.com>
2026-09-01 20:10:37 -0500

    cambio

--- src/main/java/com/uninpahu/devops/App.java
+++ src/main/java/com/uninpahu/devops/App.java
@@ -3,7 +3,7 @@ package com.uninpahu.devops;
 public class App {
     public static void main(String[] args) throws Exception {
         ServicioSalud.iniciar();
-        System.out.println("Calculadora CI iniciada. Endpoint salud en http://localhost:8080/salud");
+        System.out.println("Calculadora CI iniciada. Endpoint salud en http://localhost:8085/salud");
         Thread.currentThread().join();
     }
 }
